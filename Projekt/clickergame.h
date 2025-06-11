7726c0b2b2920d75f6245f9cb088bfec8f76b6a3
Igor-Haw <igor.hawrylczak@gmail.com>
2025-06-11 19:27:22 +0200

    Update clickergame.h

--- Projekt/clickergame.h
+++ Projekt/clickergame.h
@@ -1,17 +1,21 @@
 #ifndef CLICKERGAME_H
 #define CLICKERGAME_H
 
+#pragma once
+
 #include <SFML/Graphics.hpp>
-#include <string>
 #include "Button.h"
 
 class ClickerGame {
 public:
-    explicit ClickerGame(sf::Font& font);
+    ClickerGame(sf::Font& font);
+
     void startGame();
     void update(sf::RenderWindow& window);
+    void handleEvent(sf::Event& event, sf::RenderWindow& window);
     void draw(sf::RenderWindow& window);
     bool isActive() const;
+
     int getMoney() const;
     void setMoney(int amount);
 
@@ -31,4 +35,4 @@ private:
     Button backButton;
 };
 
-#endif
+#endif // CLICKERGAME_H
