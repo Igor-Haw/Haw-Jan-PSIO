4c0a90f9984ba326d54ce91363b249bcdf7ad4e7
Igor-Haw <igor.hawrylczak@gmail.com>
2025-06-11 19:27:57 +0200

    Update clickergame.cpp

--- Projekt/clickergame.cpp
+++ Projekt/clickergame.cpp
@@ -1,86 +1,82 @@
 #include "ClickerGame.h"
 
-ClickerGame::ClickerGame(sf::Font& font) : font(font), gameStarted(false), money(0), clickValue(1) {
-    moneyText.setFont(font);
-    moneyText.setCharacterSize(36);
-    moneyText.setFillColor(sf::Color::White);
-    moneyText.setPosition(50, 50);
+    ClickerGame::ClickerGame(sf::Font& font)
+        : font(font), gameStarted(false), money(0), clickValue(1) {
 
-    clickValueText.setFont(font);
-    clickValueText.setCharacterSize(24);
-    clickValueText.setFillColor(sf::Color::White);
-    clickValueText.setPosition(50, 100);
+        moneyText.setFont(font);
+        moneyText.setCharacterSize(36);
+        moneyText.setFillColor(sf::Color::White);
+        moneyText.setPosition(50, 50);
 
-    clickButton = Button(sf::Vector2f(300, 200), sf::Vector2f(200, 100), "CLICK!", font);
-    upgradeButton = Button(sf::Vector2f(300, 350), sf::Vector2f(200, 50), "Upgrade (10)", font);
-    backButton = Button(sf::Vector2f(300, 450), sf::Vector2f(200, 50), "Back", font);
-}
+        clickValueText.setFont(font);
+        clickValueText.setCharacterSize(24);
+        clickValueText.setFillColor(sf::Color::White);
+        clickValueText.setPosition(50, 100);
 
-void ClickerGame::startGame() {
-    gameStarted = true;
-    updateTexts();
-}
+        clickButton = Button(sf::Vector2f(300, 200), sf::Vector2f(200, 100), "Kliknij by zarobic!", font);
+        upgradeButton = Button(sf::Vector2f(300, 350), sf::Vector2f(200, 50), "Ulepsz przycisk (10$)", font);
+        backButton = Button(sf::Vector2f(300, 450), sf::Vector2f(200, 50), "Powrot", font);
+    }
 
-void ClickerGame::updateTexts() {
-    moneyText.setString("Money: " + std::to_string(money));
-    clickValueText.setString("Click value: " + std::to_string(clickValue));
-    upgradeButton = Button(sf::Vector2f(300, 350), sf::Vector2f(200, 50),
-                           "Upgrade (" + std::to_string(10 * (clickValue)) + ")", font);
-}
+    void ClickerGame::startGame() {
+        gameStarted = true;
+        updateTexts();
+    }
 
-void ClickerGame::update(sf::RenderWindow& window) {
-    if (gameStarted) {
-        if (clickButton.isMouseOver(window)) {
-            clickButton.setColor(sf::Color(100, 100, 100));
-            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-                money += clickValue;
-                updateTexts();
-            }
-        } else {
-            clickButton.setColor(sf::Color(70, 70, 70));
+    void ClickerGame::updateTexts() {
+        moneyText.setString("Saldo: " + std::to_string(money) + "$");
+        clickValueText.setString("Mnoznik klikniec: x" + std::to_string(clickValue));
+        upgradeButton = Button(sf::Vector2f(300, 350), sf::Vector2f(200, 50),
+                               "Ulepsz przycisk (" + std::to_string(500 * clickValue) + "$)", font);
+    }
+
+    void ClickerGame::handleEvent(sf::Event& event, sf::RenderWindow& window) {
+        if (!gameStarted) return;
+
+        if (clickButton.isClicked(event, window)) {
+            money += clickValue;
+            updateTexts();
         }
 
-        if (upgradeButton.isMouseOver(window)) {
-            upgradeButton.setColor(sf::Color(100, 100, 100));
-            if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && money >= 10 * clickValue) {
-                money -= 10 * clickValue;
-                clickValue++;
-                updateTexts();
-            }
-        } else {
-            upgradeButton.setColor(sf::Color(70, 70, 70));
+        if (upgradeButton.isClicked(event, window) && money >= 500 * clickValue) {
+            money -= 500 * clickValue;
+            clickValue++;
+            updateTexts();
         }
 
-        if (backButton.isMouseOver(window)) {
-            backButton.setColor(sf::Color(100, 100, 100));
-            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-                gameStarted = false;
-            }
-        } else {
-            backButton.setColor(sf::Color(70, 70, 70));
+        if (backButton.isClicked(event, window)) {
+            gameStarted = false;
         }
     }
-}
 
-void ClickerGame::draw(sf::RenderWindow& window) {
-    if (gameStarted) {
+    void ClickerGame::update(sf::RenderWindow& window) {
+        if (!gameStarted) return;
+
+        clickButton.setColor(clickButton.isMouseOver(window) ? sf::Color(100, 100, 100) : sf::Color(70, 70, 70));
+        upgradeButton.setColor(upgradeButton.isMouseOver(window) ? sf::Color(100, 100, 100) : sf::Color(70, 70, 70));
+        backButton.setColor(backButton.isMouseOver(window) ? sf::Color(100, 100, 100) : sf::Color(70, 70, 70));
+    }
+
+    void ClickerGame::draw(sf::RenderWindow& window) {
+        if (!gameStarted) return;
+
         window.draw(moneyText);
         window.draw(clickValueText);
+
         clickButton.draw(window);
         upgradeButton.draw(window);
         backButton.draw(window);
     }
-}
 
-bool ClickerGame::isActive() const {
-    return gameStarted;
-}
+    bool ClickerGame::isActive() const {
+        return gameStarted;
+    }
 
-int ClickerGame::getMoney() const {
-    return money;
-}
+    int ClickerGame::getMoney() const {
+        return money;
+    }
 
-void ClickerGame::setMoney(int amount) {
-    money = amount;
-    updateTexts();
-}
+    void ClickerGame::setMoney(int amount) {
+        money = amount;
+        updateTexts();
+    }
